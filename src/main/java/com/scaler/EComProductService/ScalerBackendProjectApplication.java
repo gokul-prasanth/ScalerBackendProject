f708bb45385d85e8f41a094d47a7e95f69486307
Gokul <prasanthgokulgp@gmail.com>
2025-12-02 19:37:23 -0500

    Initial commit

---
+++ src/main/java/com/scaler/EComProductService/ScalerBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.EComProductService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalerBackendProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/EComProductService/ScalerBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.EComProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalerBackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
