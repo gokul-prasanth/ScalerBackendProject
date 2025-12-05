3beaba71b11cecfaab9d9e7525a62e7f82872884
Gokul <prasanthgokulgp@gmail.com>
2025-12-04 22:11:47 -0500

    Added Entity classes

---
+++ src/main/java/com/scaler/EComProductService/model/Price.java
@@ -0,0 +1,16 @@
+package com.scaler.EComProductService.model;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+@Entity
+public class Price extends BaseModel {
+    private String currency;
+    private double amount;
+    private double discount;
+}
